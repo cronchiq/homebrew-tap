d1d195d80c6acf1102523806ec3b649e04a9639a
goreleaserbot <goreleaser@carlosbecker.com>
2022-01-31 16:22:23 +0100

    Brew formula update for mycli version v0.1.3

--- Formula/mycli.rb
+++ Formula/mycli.rb
@@ -5,20 +5,20 @@
 class Mycli < Formula
   desc "My CLI demo"
   homepage "https://github.com/cronchiq/mycli"
-  version "0.1.2"
+  version "0.1.3"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/cronchiq/mycli/releases/download/v0.1.2/mycli_v0.1.2_darwin_arm64.tar.gz"
-      sha256 "d3690966cc56f0da268f9180d0f93b16d0611acf38c2b79302f886a66c882943"
+      url "https://github.com/cronchiq/mycli/releases/download/v0.1.3/mycli_v0.1.3_darwin_arm64.tar.gz"
+      sha256 "b96fea994ee5854a651aaab891afec620baa1908da6b711729836574d0670d48"
 
       def install
         bin.install "mycli"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/cronchiq/mycli/releases/download/v0.1.2/mycli_v0.1.2_darwin_amd64.tar.gz"
-      sha256 "d7eb720836737d166286930338392fd9e5df0f7cf570906b71cbdb66a2de8673"
+      url "https://github.com/cronchiq/mycli/releases/download/v0.1.3/mycli_v0.1.3_darwin_amd64.tar.gz"
+      sha256 "cc0bc4ae017bd7d2aa0c080f9af2c47db7476ef06dacf78f7f3aecce7506937f"
 
       def install
         bin.install "mycli"
@@ -28,16 +28,16 @@ class Mycli < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/cronchiq/mycli/releases/download/v0.1.2/mycli_v0.1.2_linux_amd64.tar.gz"
-      sha256 "a4e51af3f8246e36d8c6a932fa909ab80fe5285c46a71fc9f0499c141cfc3138"
+      url "https://github.com/cronchiq/mycli/releases/download/v0.1.3/mycli_v0.1.3_linux_amd64.tar.gz"
+      sha256 "adfed93c2d3e4e169a43a98f2267bfa4c7f38835337c71ec95bc3c17ceda98d4"
 
       def install
         bin.install "mycli"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/cronchiq/mycli/releases/download/v0.1.2/mycli_v0.1.2_linux_arm64.tar.gz"
-      sha256 "e46540ecfb4f73c23ff238e72d63235016f69932fa56d220008297fc608b81a6"
+      url "https://github.com/cronchiq/mycli/releases/download/v0.1.3/mycli_v0.1.3_linux_arm64.tar.gz"
+      sha256 "0a6a1e07bc3b47d5a1779c115c7c3d3ff98523755f83ec8c253a33c411aaaa0a"
 
       def install
         bin.install "mycli"
